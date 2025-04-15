ffc747eeaa4428c55eaf8a79001724a89c7d3077
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-15 19:31:00 +0600

    adjacent list added

--- src/Adjacency/List/AddRmAdjList.java
+++ src/Adjacency/List/AddRmAdjList.java
@@ -27,4 +27,40 @@ public class AddRmAdjList {
         graphArray[sourceVertex] = node;
     }
 
+    // function to add vertex;
+    public void addVertex(int common, int source, int destination)
+    {
+        addEdges(source, common);
+        addEdges(common, destination);
+    }
+    //print graph;
+
+    public void printGraph()
+    {
+        for(int i = 0; i < v; i++)
+        {
+            System.out.print(i + " : ");
+            AddRmAdjLstNode temp = graphArray[i];
+
+            while (temp != null)
+            {
+                System.out.print( " -> " + temp.Vertex + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int v = 4;
+        AddRmAdjList graph = new AddRmAdjList(v);
+        graph.addEdges(0,1);
+        graph.addEdges(0,2);
+        graph.addEdges(0,3);
+        graph.addEdges(1,2);
+        graph.addEdges(2,3);
+
+        graph.printGraph();
+    }
+
 }
