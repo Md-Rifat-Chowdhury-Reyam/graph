56091719738c5bd231bab488ff924dfb4f3b7905
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-15 21:48:33 +0600

    adjacent list

--- src/Adjacency/List/AddRmAdjList.java
+++ src/Adjacency/List/AddRmAdjList.java
@@ -47,12 +47,48 @@ public class AddRmAdjList {
                 System.out.print( " -> " + temp.Vertex + " ");
                 temp = temp.next;
             }
-            System.out.println();
+            System.out.println(" ");
+        }
+    }
+    //delete vertex;
+    public void deleteVertex(int nodeKey)
+    {
+        for(int i = 0; i < v; i++)
+        {
+            AddRmAdjLstNode curr =  graphArray[i], prev = null;
+
+            if(i == nodeKey)
+            {
+                graphArray[i] = null;
+            }
+
+            else {
+
+                while(curr != null)
+                {
+                    if(curr.Vertex == nodeKey)
+                    {
+                        if(prev == null)
+                        {
+                            graphArray[i] = curr.next;
+                        }
+                        else
+                        {
+                            prev.next = curr.next;
+                        }
+                        break;
+
+                    }
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+
         }
     }
 
     public static void main(String[] args) {
-        int v = 4;
+        int v = 6;
         AddRmAdjList graph = new AddRmAdjList(v);
         graph.addEdges(0,1);
         graph.addEdges(0,2);
@@ -61,6 +97,17 @@ public class AddRmAdjList {
         graph.addEdges(2,3);
 
         graph.printGraph();
+        System.out.println("\n");
+
+        graph.addVertex(5,3,2);
+        graph.printGraph();
+        System.out.println("\n");
+
+        graph.deleteVertex(5);
+        graph.printGraph();
+
+
+
     }
 
 }
