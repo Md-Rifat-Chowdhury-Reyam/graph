29c6f53a8cf0cd296781e252b4efcbf47aed6ed7
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-20 23:23:54 +0600

    bfs updated

--- src/BFS/Example1.java
+++ src/BFS/Example1.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Example1 {
+
     //create a source node
     public static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adjMat)
     {
@@ -15,36 +16,33 @@ public class Example1 {
 
         //create an array to store the traversal;
         ArrayList<Integer> res = new ArrayList<>();
-
         //create a queue  for bfs;
         Queue<Integer> qu = new LinkedList<>();
 
         //Initially mark the vertices they are not visited by boolean array;
-        Boolean[] visited = new Boolean [v];
-        visited[s] = true;// Mark the source node or vertex as visited;
-        qu.add(s); // add source node in queue;
+        boolean[] visited = new boolean [v];
+        // Mark the source node or vertex as visited;
+        visited[s] = true;
+        // add source node in queue;
+        qu.add(s);
 
         //Iterate over the queue;
         //while use for check first condition;
-        while(!qu.isEmpty())
-        {
-            int curr = qu.poll(); // current value of vertex dequeue from queue;
-            res.add(curr); // store in res array;
+        while (!qu.isEmpty()) {
 
-            //get all adjacent vertices of the dequeue;
-            for(int x : adjMat.get(curr))
-            {
-                if(!visited[x])// visited node inverted
-                {
-                    visited[x] = true; // visited node true then -->
-                    // add in queue;
+            // Dequeue a vertex from queue and store it
+            int curr = qu.poll();
+            res.add(curr);
+
+            // Get all adjacent vertices of the dequeued
+            // vertex curr If an adjacent has not been
+            // visited, mark it visited and enqueue it
+            for (int x : adjMat.get(curr)) {
+                if (!visited[x]) {
+                    visited[x] = true;
                     qu.add(x);
                 }
-                System.out.println();
-
             }
-            System.out.println();
-
         }
 
         return res;
@@ -54,15 +52,17 @@ public class Example1 {
 
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        adj.add(new ArrayList<>(Arrays.asList(0,1)));
-        adj.add(new ArrayList<>(Arrays.asList(0,3)));
-        adj.add(new ArrayList<>(Arrays.asList(0,2,3)));
-        adj.add(new ArrayList<>(Arrays.asList(1,4,3)));
+
+        adj.add(new ArrayList<>(Arrays.asList(1, 2)));
+        adj.add(new ArrayList<>(Arrays.asList(0, 2, 3)));
+        adj.add(new ArrayList<>(Arrays.asList(0, 4)));
+        adj.add(new ArrayList<>(Arrays.asList(1,4)));
+        adj.add(new ArrayList<>(Arrays.asList(2,3)));
 
         ArrayList<Integer> adjM = bfs(adj);
         for(int i : adjM)
         {
-            System.out.println(i + " ");
+            System.out.print(i + " ");
         }
     }
 
