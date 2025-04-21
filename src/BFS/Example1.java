f1d9db07950bce73cb0db7d7da501401ab71b17d
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-21 16:02:45 +0600

    bfs with given source node

--- src/BFS/Example1.java
+++ src/BFS/Example1.java
@@ -1,69 +1,30 @@
 package BFS;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Example1 {
 
-    //create a source node
-    public static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adjMat)
+    public static ArrayList<Integer> bfs (ArrayList<ArrayList<Integer>> adjMat)
     {
-        //vertex size;
         int v = adjMat.size();
-        int s = 0; // initialize source to zero;
+        int s = 0;
 
-        //create an array to store the traversal;
-        ArrayList<Integer> res = new ArrayList<>();
-        //create a queue  for bfs;
+        ArrayList<Integer> reStore = new ArrayList<>();
         Queue<Integer> qu = new LinkedList<>();
 
-        //Initially mark the vertices they are not visited by boolean array;
-        boolean[] visited = new boolean [v];
-        // Mark the source node or vertex as visited;
+        boolean[] visited = new boolean[v];
         visited[s] = true;
-        // add source node in queue;
         qu.add(s);
 
-        //Iterate over the queue;
-        //while use for check first condition;
-        while (!qu.isEmpty()) {
 
-            // Dequeue a vertex from queue and store it
-            int curr = qu.poll();
-            res.add(curr);
 
-            // Get all adjacent vertices of the dequeued
-            // vertex curr If an adjacent has not been
-            // visited, mark it visited and enqueue it
-            for (int x : adjMat.get(curr)) {
-                if (!visited[x]) {
-                    visited[x] = true;
-                    qu.add(x);
-                }
-            }
-        }
 
-        return res;
+        return reStore;
     }
 
     public static void main(String[] args) {
 
-
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        adj.add(new ArrayList<>(Arrays.asList(1, 2)));
-        adj.add(new ArrayList<>(Arrays.asList(0, 2, 3)));
-        adj.add(new ArrayList<>(Arrays.asList(0, 4)));
-        adj.add(new ArrayList<>(Arrays.asList(1,4)));
-        adj.add(new ArrayList<>(Arrays.asList(2,3)));
-
-        ArrayList<Integer> adjM = bfs(adj);
-        for(int i : adjM)
-        {
-            System.out.print(i + " ");
-        }
     }
-
 }
