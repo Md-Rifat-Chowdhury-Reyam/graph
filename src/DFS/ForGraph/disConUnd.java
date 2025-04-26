288f9e561aa726b4d2702365046455a1cd2634b1
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-26 15:57:37 +0600

    dfs add edges

--- src/DFS/ForGraph/disConUnd.java
+++ src/DFS/ForGraph/disConUnd.java
@@ -5,68 +5,13 @@ import java.util.ArrayList;
 public class disConUnd {
     public static void main(String[] args) {
 
-        int v = 6;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-        //initialize adjacent list
-        for(int i = 0; i < v; i++)
-        {
-            adj.add(new ArrayList<>());
-            System.out.print(adj.get(i));
-
-        }
-        System.out.println();
-
-        int[][] edges = {{ 1, 2 }, { 2, 0 }, { 0, 3 }, { 4, 5 } };
-        for (int[] i : edges)
-        {
-            addEdges(adj, i[0], i[1]);
-
-        }
-
-        ArrayList<Integer> res = dfsM(adj);
-        for(int i : res)
-        {
-            System.out.print( i + " ");
-        }
-
-
     }
 
-    public static void addEdges(ArrayList<ArrayList<Integer>> adjM, int s, int n)
+    public static void addEdges(ArrayList<ArrayList<Integer>> adj, int a, int b)
     {
-        adjM.get(s).add(n);
-        adjM.get(n).add(s);
+        adj.get(a).add(b);
+        adj.get(b).add(a);
     }
 
-    public static void recursive(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int s, ArrayList<Integer> res)
-    {
-        visited[s] = true;
-        res.add(s);
 
-       for(int i : adj.get(s))
-       {
-           if(!visited[i])
-           {
-               recursive(adj,visited, i, res);
-           }
-       }
-    }
-
-    public static ArrayList<Integer> dfsM(ArrayList<ArrayList<Integer>> adj)
-    {
-        boolean[] visited = new boolean[adj.size()];
-        ArrayList<Integer> res = new ArrayList<>();
-
-        for (int i = 0; i < adj.size(); i++ )
-        {
-            if(!visited[i])
-            {
-                recursive(adj,visited, i, res);
-            }
-        }
-
-        return res;
-
-    }
 }
